d2e1f645f3bfc69114f18e7c9a8f52ccfe4b7e0c
Prabhurbvn <prabhurbvn@gmail.com>
2022-07-19 11:36:34 +0530

    Third_commit
    modify add function

--- FirstPgm.py
+++ FirstPgm.py
@@ -1,13 +1,13 @@
 class Sample:
   
-  def add(self,a,b):
-      c=a+b
-      print("add result=",c)
+  def add(self,a=5,b=5,c):
+      d=a+b+c
+      print("add result=",d)
       
   def sub(self,a,b):
     c=a-b
     print("sub result=",c)
       
 s=Sample()
-s.add()
+s.add(10,20,30)
 s.sub()
