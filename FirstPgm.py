423ef21d207df8674b35fee4636354b24610fa9a
Prabhurbvn <prabhurbvn@gmail.com>
2022-07-19 11:38:04 +0530

    Fourth_commit
    added multiplication function

--- FirstPgm.py
+++ FirstPgm.py
@@ -7,7 +7,12 @@ class Sample:
   def sub(self,a,b):
     c=a-b
     print("sub result=",c)
+    
+  def mul(self, a,b)
+    c=a*b
+    print("mul result=",c)
       
 s=Sample()
 s.add(10,20,30)
-s.sub()
+s.sub(20,10)
+s.mul(6,6)
